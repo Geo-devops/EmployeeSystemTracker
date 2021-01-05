a782c036d8c4b259bf2490c9cdb89db013dceca8
GBondarek <georgettebondarek@gmail.com>
2021-01-05 18:50:33 -0500

    update

--- index.js
+++ index.js
@@ -29,4 +29,51 @@ const connection = mysql.createConnection({
                 }
                 console.log(chalk.red(data))
             });
+        
+            inquirer.prompt([{
+                message: 'What employee do you want to look at?',
+                type: 'list',
+                name: 'choice',
+
+                Choices: [
+                'View All Employees',
+               'View All Employees in Departments',
+               'View All Roles',
+
+               'Add employee',
+               'Add role',
+               'Add Department',
+               'View all Departments',
+               'End Program'
+                ]
+            }])
+        
+        .then((answers) => {
+            if (answers.choice === 'view all employees') {
+                viewAllEmployees(); 
+            } else if (answers.choice ==='View all Employees in Departments'){
+                viewByDepartment();
+            }     
+            else if (answers.choice === 'Exit Program') {
+                exit();
+                  }
+             })
+                      
         }
+
+        initialPrompt();
+
+        function viewAllEmployees() {
+            connection.query('SELECT employee.first_name, employee.last_name, role.title AS Title, role.salary AS Salary,deparment.name AS Department
+            FROM employee
+                INNER JOIN role on employee.role_id=role.role_id
+                INNER JOIN department ON employee.role_id=department.department_id', (err, result) => {
+                    if (err) throw err;
+                    console.table(results);
+
+                })
+        initialPrompt();
+            }
+            
+            
+            )
